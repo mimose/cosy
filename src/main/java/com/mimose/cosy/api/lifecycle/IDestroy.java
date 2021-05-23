d8852d75380b0659414347b3f305da9fe455d579
mimose <mimosecy@gmail.com>
2021-05-23 16:44:52 +0800

    develop: perfect lifecycle api

---
+++ src/main/java/com/mimose/cosy/api/lifecycle/IDestroy.java
@@ -0,0 +1,11 @@
+package com.mimose.cosy.api.lifecycle;
+
+/**
+ * @author mimose
+ * @description destroy when application dead
+ * @date 2021/5/23
+ */
+public interface IDestroy {
+
+    void destroy();
+}
---
+++ src/main/java/com/mimose/cosy/api/lifecycle/IStartup.java
@@ -0,0 +1,11 @@
+package com.mimose.cosy.api.lifecycle;
+
+/**
+ * @author mimose
+ * @description run when start application
+ * @date 2021/5/23
+ */
+public interface IStartup {
+
+    void run();
+}
